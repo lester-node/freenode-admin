684d9ce6a655475f0cf4d8ab2f3d9bc18c907761
Lester <1>
2022-05-16 01:17:11 +0800

    fix: 修改tab

--- src/pages/classify/index.tsx
+++ src/pages/classify/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import MarkdownIt from 'markdown-it'
-import MdEditor from 'react-markdown-editor-lite'
+import Editor, { Plugins } from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css'
 
 const mdParser = new MarkdownIt()
+Editor.use(Plugins.TabInsert, {
+  /**
+   * 用户按下 Tab 键时输入的空格的数目
+   * 特别地，1 代表输入一个'\t'，而不是一个空格
+   * 默认值是 1
+   */
+  tabMapValue: 4
+})
 
 export default () => {
   function handleEditorChange ({ html, text }) {
@@ -11,8 +19,19 @@ export default () => {
   }
 
   return (
-    <MdEditor
+    // <MdEditor
+    //   style={{ height: '500px' }}
+    //   renderHTML={(text) => mdParser.render(text)}
+    //   onChange={handleEditorChange}
+    // />
+    <Editor
       style={{ height: '500px' }}
+      onImageUpload={(file) => {
+        console.log('file', file)
+        return new Promise((resolve) => {
+          resolve('localhost:3000/upload_5c07cadc18053db7ae008f5508305484.png')
+        })
+      }}
       renderHTML={(text) => mdParser.render(text)}
       onChange={handleEditorChange}
     />
